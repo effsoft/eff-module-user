06dee9ca5b4159f279eb0576639a81f16f0b325e
effsoft <herilan@hotmail.com>
2019-03-22 21:58:49 +0800

    commit

--- src/modules/admin/controllers/ManageController.php
+++ src/modules/admin/controllers/ManageController.php
@@ -25,18 +25,8 @@ class ManageController extends EffController{
     }
 
     public function actionIndex(){
-        $query = UserModel::find()->orderBy(['_id' => SORT_DESC]);
-        $totalCount = $query->count();
-        $pagination = new Pagination(['totalCount' => $totalCount]);
-        // $pagination->setPageSize(1);
-        $users = $query->offset($pagination->offset)
-            ->limit($pagination->limit)
-            ->all();
-            // print_r($users);
+        
         return $this->render('//user/admin/manage/index',[
-            'pagination' => $pagination,
-            'totalCount' => $totalCount,
-            'users' => $users,
         ]);
     }
 }
